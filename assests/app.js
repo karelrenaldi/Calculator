8ea1351ba70c46792737b2ca3fa6584b03f593d6
Karel Renaldi <karelrenaldi8@gmail.com>
2020-03-24 08:54:09 +0700

    first commit (finish html and css)

---
+++ assests/app.js
@@ -0,0 +1,7 @@
+const key = document.querySelector('.key');
+const result = document.querySelector('#result');
+key.addEventListener('click',(e)=>{
+    if(e.target.tagName === 'SPAN'){
+        
+    }
+})
